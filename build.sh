a0bf1e94a092e65ee61e3a35d453be3e6314e8ea
Yajat Sharma <yajat004@gmail.com>
2026-07-27 22:48:54 +0530

    Update dataset download link

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 echo "Downloading similarity matrix..."
 
 wget -O simi_list2.pkl \
-https://huggingface.co/Yajat004/movie-recommender-system/resolve/main/simi_list2.pkl
+https://huggingface.co/datasets/Yajat004/movie-recommender-system/resolve/main/simi_list2.pkl
