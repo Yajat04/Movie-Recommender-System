58dbd0d1c56321053774375619dfb7c1cf95d773
Yajat Sharma <yajat004@gmail.com>
2026-08-14 00:37:17 +0530

    Corrected name of simi file

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 echo "Downloading similarity matrix..."
 
-wget -O simi_list2.pkl \
+wget -O simif16.pkl\
 https://huggingface.co/datasets/Yajat004/movie-recommender-system/resolve/main/simif16.pkl
