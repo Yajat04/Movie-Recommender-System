6e3629a90128168a3345974fc3b01ed087fb6c82
Yajat Sharma <yajat004@gmail.com>
2026-08-13 21:56:54 +0530

    Updated files and added env var file

--- app.py
+++ app.py
@@ -1,96 +1,272 @@
+import asyncio
+import os
+import pickle
+
+import aiohttp
+import numpy as np
 import pandas as pd
 import streamlit as st
-st.title('Movie Recommender System')
-import pickle
-# file = open("movies.pkl", 'rb')
-# df = pickle.load(file)
-# titles = df['title'].values
-# movies_list = st.selectbox('Select Movie', titles)
 
-import requests
-import os
-#$env:TMDB_API_KEY="your_api" in terminal
+
+st.title("Movie Recommender System")
+
+
+# --------------------------------------------------
+# API KEY
+# --------------------------------------------------
+
+from dotenv import load_dotenv
+load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
-#st.write("API Key loaded:", API_KEY is not None) ,tells if api loaded successfully
-#Above is done to secure api
 
-# import sys
-# st.write("Python:", sys.executable)
-# st.write("API_KEY:", API_KEY)
 
-def fetch_url_poster(movie_id):
+# --------------------------------------------------
+# LOAD DATA ONLY ONCE
+# --------------------------------------------------
+
+@st.cache_resource
+def load_data():
+
+    with open("movies_dict.pkl", "rb") as file:
+        movies_dict = pickle.load(file)
+
+    movies = pd.DataFrame(movies_dict)
+
+    with open("simi_list2.pkl", "rb") as file:
+        simi_list = pickle.load(file)
+
+    simi = np.array(simi_list)
+
+    return movies, simi
+
+
+movies, simi = load_data()
+
+titles = movies["title"].values
+
+
+# --------------------------------------------------
+# ASYNC TMDB POSTER FETCH
+# --------------------------------------------------
+
+async def fetch_poster(session, movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
+
+
     try:
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
-        response = requests.get(url, timeout=10)
-        #st.write(response.status_code)
-        data = response.json()
-        #st.write(data)
-        #st.write("https://image.tmdb.org/t/p/w500" + data["poster_path"]), to know if it really fetches or not
-        return "https://image.tmdb.org/t/p/w500" + data["poster_path"]
+
+        async with session.get(
+            url,
+            timeout=aiohttp.ClientTimeout(total=10)
+        ) as response:
+
+            if response.status != 200:
+                return "no_poster.png"
+
+            data = await response.json()
+
+            poster_path = data.get("poster_path")
+
+            if poster_path is None:
+                return "no_poster.png"
+
+            return f"https://image.tmdb.org/t/p/w500{poster_path}"
 
     except Exception:
-        #used to return null if the poster isn't available
         return "no_poster.png"
 
-#Function to fetch Recommended Movies using movie df and similarity matrix
-def recommend(mov_input):
-    mov_index = movies[movies['title'] == mov_input].index[0]
-    simi_row = simi[mov_index]
-    mov_list = sorted(list(enumerate(simi_row)), key = lambda x : x[1], reverse = True)[1:6]
 
-    recommended = [] #To store recommended movie titles
-    poster_url = [] #To store posters of the recommended movies
-    for tup in mov_list:
-        MovId = fetch_url_poster(movies.iloc[tup[0]]['id'])
-        #fetch 'movie id' from movie df using index, and referenced it to find fetch poster
-        poster_url.append(MovId)
+# --------------------------------------------------
+# FETCH ALL 5 POSTERS CONCURRENTLY
+# --------------------------------------------------
 
-        recommended.append(movies.iloc[tup[0]]['title'])
+async def fetch_posters(movie_ids):
 
-    return poster_url, recommended
+    async with aiohttp.ClientSession() as session:
 
-#Importing Movies dataframe(new_df)
-file = open("movies_dict.pkl", 'rb')
-Movies_dict = pickle.load(file)
-movies = pd.DataFrame(Movies_dict)
-titles = movies['title'].values
+        tasks = [
+            fetch_poster(session, movie_id)
+            for movie_id in movie_ids
+        ]
 
-movie = st.selectbox('Discover the Top 5 similar movies based on your selection! 🔥',
-                     titles,
-                     index=None,
-                     placeholder="Choose a movie...")
-
-#Importing Similarity Matrix
-fileSimi = open("simi_list2.pkl", 'rb')
-simi_list = pickle.load(fileSimi)
-
-import numpy as np
-simi = np.array(simi_list)
+        return await asyncio.gather(*tasks)
 
 
-if st.button('🎬 Get Recommendations!'):
+# --------------------------------------------------
+# RECOMMEND
+# --------------------------------------------------
+
+def recommend(movie_input):
+
+    movie_index = movies[movies["title"] == movie_input].index[0]
+
+    similarity_row = simi[movie_index]
+
+    movie_list = sorted(
+        list(enumerate(similarity_row)),
+        key=lambda x: x[1],
+        reverse=True
+    )[1:6]
+
+    recommended_movies = [
+        movies.iloc[tup[0]]["title"]
+        for tup in movie_list
+    ]
+
+    movie_ids = [
+        movies.iloc[tup[0]]["id"]
+        for tup in movie_list
+    ]
+
+    # Run the 5 API requests concurrently
+    poster_urls = asyncio.run(
+        fetch_posters(movie_ids)
+    )
+
+    return poster_urls, recommended_movies
+
+
+# --------------------------------------------------
+# STREAMLIT UI
+# --------------------------------------------------
+
+movie = st.selectbox(
+    "Discover the Top 5 similar movies based on your selection! 🔥",
+    titles,
+    index=None,
+    placeholder="Choose a movie..."
+)
+
+
+if st.button("🎬 Get Recommendations!"):
+
     if movie is None:
+
         st.warning("Please select a movie first!")
+
     else:
+
         poster, recommended_movies = recommend(movie)
+
         col1, col2, col3, col4, col5 = st.columns(5)
+
         with col1:
             st.image(poster[0])
             st.text(recommended_movies[0])
+
         with col2:
             st.image(poster[1])
             st.text(recommended_movies[1])
+
         with col3:
             st.image(poster[2])
             st.text(recommended_movies[2])
+
         with col4:
             st.image(poster[3])
             st.text(recommended_movies[3])
+
         with col5:
             st.image(poster[4])
             st.text(recommended_movies[4])
 
-# Versions at time of pickling
-# numpy : 2.1.3
-# pandas : 2.2.3
-# sklearn : 1.6.1
+
+
+
+
+# import pandas as pd
+# import streamlit as st
+# st.title('Movie Recommender System')
+# import pickle
+# # file = open("movies.pkl", 'rb')
+# # df = pickle.load(file)
+# # titles = df['title'].values
+# # movies_list = st.selectbox('Select Movie', titles)
+#
+# import requests
+# import os
+# #$env:TMDB_API_KEY="your_api" in terminal
+# API_KEY = os.getenv("TMDB_API_KEY")
+# #st.write("API Key loaded:", API_KEY is not None) ,tells if api loaded successfully
+# #Above is done to secure api
+#
+# # import sys
+# # st.write("Python:", sys.executable)
+# # st.write("API_KEY:", API_KEY)
+#
+# def fetch_url_poster(movie_id):
+#     try:
+#         url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
+#         response = requests.get(url, timeout=10)
+#         #st.write(response.status_code)
+#         data = response.json()
+#         #st.write(data)
+#         #st.write("https://image.tmdb.org/t/p/w500" + data["poster_path"]), to know if it really fetches or not
+#         return "https://image.tmdb.org/t/p/w500" + data["poster_path"]
+#
+#     except Exception:
+#         #used to return null if the poster isn't available
+#         return "no_poster.png"
+#
+# #Function to fetch Recommended Movies using movie df and similarity matrix
+# def recommend(mov_input):
+#     mov_index = movies[movies['title'] == mov_input].index[0]
+#     simi_row = simi[mov_index]
+#     mov_list = sorted(list(enumerate(simi_row)), key = lambda x : x[1], reverse = True)[1:6]
+#
+#     recommended = [] #To store recommended movie titles
+#     poster_url = [] #To store posters of the recommended movies
+#     for tup in mov_list:
+#         MovId = fetch_url_poster(movies.iloc[tup[0]]['id'])
+#         #fetch 'movie id' from movie df using index, and referenced it to find fetch poster
+#         poster_url.append(MovId)
+#
+#         recommended.append(movies.iloc[tup[0]]['title'])
+#
+#     return poster_url, recommended
+#
+# #Importing Movies dataframe(new_df)
+# file = open("movies_dict.pkl", 'rb')
+# Movies_dict = pickle.load(file)
+# movies = pd.DataFrame(Movies_dict)
+# titles = movies['title'].values
+#
+# movie = st.selectbox('Discover the Top 5 similar movies based on your selection! 🔥',
+#                      titles,
+#                      index=None,
+#                      placeholder="Choose a movie...")
+#
+# #Importing Similarity Matrix
+# fileSimi = open("simi_list2.pkl", 'rb')
+# simi_list = pickle.load(fileSimi)
+#
+# import numpy as np
+# simi = np.array(simi_list)
+#
+#
+# if st.button('🎬 Get Recommendations!'):
+#     if movie is None:
+#         st.warning("Please select a movie first!")
+#     else:
+#         poster, recommended_movies = recommend(movie)
+#         col1, col2, col3, col4, col5 = st.columns(5)
+#         with col1:
+#             st.image(poster[0])
+#             st.text(recommended_movies[0])
+#         with col2:
+#             st.image(poster[1])
+#             st.text(recommended_movies[1])
+#         with col3:
+#             st.image(poster[2])
+#             st.text(recommended_movies[2])
+#         with col4:
+#             st.image(poster[3])
+#             st.text(recommended_movies[3])
+#         with col5:
+#             st.image(poster[4])
+#             st.text(recommended_movies[4])
+#
+# # Versions at time of pickling
+# # numpy : 2.1.3
+# # pandas : 2.2.3
+# # sklearn : 1.6.1
