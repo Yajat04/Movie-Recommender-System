991428a39fa59f8d32cfb50476ab0b145382b4e4
Yajat Sharma <yajat004@gmail.com>
2026-08-15 03:41:58 +0530

    Final Changes

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Downloading similarity matrix..."
+echo "Downloading movies dataframe and similarity matrix..."
 
-wget -O simif16.pkl \
-https://huggingface.co/datasets/Yajat004/movie-recommender-system/resolve/main/simif16.pkl
+
+wget -O movies.pkl \
+https://huggingface.co/datasets/Yajat004/movie-recommender-system/resolve/main/movies.pkl
+
+wget -O similarity.pkl \
+https://huggingface.co/datasets/Yajat004/movie-recommender-system/resolve/main/similarity.pkl
+
